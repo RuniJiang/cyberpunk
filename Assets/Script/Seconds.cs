550dffd079110b23ba2384bfb4b455395542298f
RuniJiang <jiangruni2020@163.com>
2020-10-29 16:11:49 +0800

    such a big work

---
+++ Assets/Script/Seconds.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+//如何动态的将脚本挂到游戏物体上？
+
+public class Seconds : MonoBehaviour
+{
+    
+
+    GameObject hour,minute,second;
+    const float degreeperHour = 30f;
+    const float degreeperMinute = 6f;
+    const float degreeperSecond = 6f;
+
+    private void Awake()
+    {
+        //Debug.Log(DateTime.Now.Hour);
+
+        //初始化钟表时间
+        float extra_min = DateTime.Now.Second / 60f * degreeperMinute;
+        float extra_hour = DateTime.Now.Minute / 60f * degreeperHour;
+        
+
+        hour = transform.Find("center_hour").gameObject;
+        hour.transform.Rotate(new Vector3(0f, 0f, - DateTime.Now.Hour * degreeperHour - extra_hour));
+
+        minute = transform.Find("center_min").gameObject;
+        minute.transform.Rotate(new Vector3(0f, 0f, -DateTime.Now.Minute * degreeperMinute - extra_min));
+
+        second = transform.Find("center_second").gameObject;
+        second.transform.Rotate(new Vector3(0f, 0f, -DateTime.Now.Second * degreeperSecond));
+
+
+    }
+
+    private void Start()
+    {
+        InvokeRepeating("Continuous", 0, 1);
+
+    }
+    //int i = 0;
+    void Update()
+    {
+        
+    }
+    void Continuous()
+    {
+        //if (Time.time - i >= 1)
+        // second.transform.localEulerAngles = Vector3.Lerp(second.transform.localEulerAngles, new Vector3(0, 0, degreeperHour * 6), Time.deltaTime * 1f);
+
+        {
+            hour.transform.Rotate(new Vector3(0f, 0f, -degreeperHour / 3600));
+            minute.transform.Rotate(new Vector3(0f, 0f, -degreeperMinute / 600));
+            second.transform.Rotate(new Vector3(0f, 0f, -degreeperSecond));
+            //i++;
+        }
+    }
+}
---
+++ Assets/TeacherMethod/clock.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clock : MonoBehaviour
+{
+    Transform _second, _minute, _hour;
+    private void Awake()
+    {
+        _second = transform.Find("secondHand");
+        _minute = transform.Find("minHand");
+        _hour = transform.Find("hourHand");
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
